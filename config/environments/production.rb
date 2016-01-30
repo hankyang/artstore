a6d57dabb6083ccc22ff0090a612d325c2bc9612
hank.kh.yang <hank.kh.yang@gmail.com>
2016-01-30 22:09:57 +0800

    update heroku domain name

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,7 +83,7 @@ Rails.application.configure do
   end
 
   # https://sleepy-fjord-12874.herokuapp.com/
-  config.action_mailer.default_url_options = { host: "https://sleepy-fjord-12874.herokuapp.com/" } 
+  config.action_mailer.default_url_options = { host: "http://artstorehcb.herokuapp.com/" } 
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
